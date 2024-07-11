a265707e284e8996b380663257886c29350b8040
Sayan B. <sayan.badarchy@gmail.com>
2024-07-11 21:23:31 +0400

    review: refactoring

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,11 +1,41 @@
 import UIKit
 
 
-extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+final class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosNames: [String] = Array(0..<20).map{"\($0)"}
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        if indexPath.row >= photosNames.count {
+            return
+        }
+        
+        let photoName = photosNames[indexPath.row]
+        cell.backgoundImage?.image = UIImage(named: photoName) ?? UIImage()
+        cell.dateLabel?.text = dateFormatter.string(from: Date())
+        
+        let imageName: String = indexPath.row % 2 == 0 ? "fav_on" : "fav_off"
+        cell.likeButton?.setImage(UIImage(named: imageName), for: .normal)
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosNames[indexPath.row]) else {
             return 0
@@ -34,46 +64,5 @@ extension ImagesListViewController: UITableViewDataSource {
         
         configCell(for: imageListCell, with: indexPath)
         return imageListCell
-    }
-    
-}
-
-
-class ImagesListViewController: UIViewController {
-
-    @IBOutlet private var tableView: UITableView!
-    
-    private let photosNames: [String] = Array(0..<20).map{"\($0)"}
-    private lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = 200
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    }
-    
-    
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        if indexPath.row >= photosNames.count {
-            return
-        }
-        
-        let photoName = photosNames[indexPath.row]
-        cell.backgoundImage?.image = UIImage(named: photoName) ?? UIImage()
-        cell.dateLabel?.text = dateFormatter.string(from: Date())
-        
-        if indexPath.row % 2 == 0 {
-            cell.likeButton?.setImage(UIImage(named: "fav_on"), for: UIControl.State.normal)
-        } else {
-            cell.likeButton?.setImage(UIImage(named: "fav_off"), for: UIControl.State.normal)
-        }
     }
 }
