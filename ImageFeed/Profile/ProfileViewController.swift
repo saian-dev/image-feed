86580114e6cfa8650d024cb31444df8b3bb1e929
Sayan B <sayan.badarchy@gmail.com>
2024-08-02 22:15:24 +0400

    navigation single image view

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Саян Бадарчы on 28.07.2024.
+//
+
+import Foundation
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
